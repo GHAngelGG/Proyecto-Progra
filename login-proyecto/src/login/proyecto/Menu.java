9d382a9d765c94b5e49ff5ea7d79fe19ff5216cf
GHAngelGG <joseangmil100@gmail.com>
2026-05-06 22:01:27 -0600

    Pantalla configuracion de cuota

--- login-proyecto/src/login/proyecto/Menu.java
+++ login-proyecto/src/login/proyecto/Menu.java
@@ -79,7 +79,7 @@ public class Menu extends JFrame {
 
         grid.add(moduleButton("Register Owner",        "Add or view property owners",       this::openRegisterOwner));
         grid.add(moduleButton("Register Payment",      "Record monthly fee payments",        this::openRegisterPayment));
-        grid.add(moduleButton("Fee Configuration",     "Update the monthly fee amount",      this::openComingSoon));
+        grid.add(moduleButton("Fee Configuration",     "Update the monthly fee amount",      this::openFeeConfiguration));
         grid.add(moduleButton("Account Statement",     "View payment history by house",      this::openComingSoon));
         grid.add(moduleButton("General Report",        "Summary of condominium income",      this::openComingSoon));
         grid.add(moduleButton("Delinquent Houses",     "List of houses with pending fees",   this::openComingSoon));
@@ -141,6 +141,10 @@ public class Menu extends JFrame {
 
     private void openRegisterPayment() {
         new RegisterPayment(this).setVisible(true);
+    }
+
+    private void openFeeConfiguration() {
+        new FeeConfiguration(this).setVisible(true);
     }
 
     private void openComingSoon() {
